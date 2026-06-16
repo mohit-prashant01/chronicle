577713f515fb41b541d02debfb0d3c20298dea58
Mohit Prashant <mohit.prashant16@gmail.com>
2026-06-16 22:54:35 +0530

    Environment Configuration + PostgreSQL Connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from sqlalchemy import text
 
-app = FastAPI()
+
+from app.db.database import engine
+
+app = FastAPI(
+    title="Chronicle API"
+)
 
 @app.get("/")
 async def root()-> dict[str,str]:
     return{"message":"Chronicles API is running"}
+
+
+@app.get("/health/db")
+async def check_db():
+    async with engine.begin() as conn:
+        await conn.execute(
+            text("SELECT 1")
+        )
+
+    return {
+    "database":"connected"
+    }
